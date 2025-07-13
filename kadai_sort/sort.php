7e457b75a33b8b50385506cb8a5f20823bf99bb2
so-ne <chihiron2007@gmail.com>
2025-07-13 12:40:45 +0900

    kadai_sortをプッシュ

---
+++ kadai_sort/sort.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        
+        $nums = [15, 4, 18, 23, 10 ];
+
+        function sort_2way($array, $order) {
+          if ($order === TRUE) {
+            echo '昇順にします<br>';
+            sort($nums, $array);
+            foreach ($nums as $num) {
+              echo $num . '<br>';
+            }
+          } else {
+            echo '降順にします<br>';
+            rsort($nums, $array);
+            foreach ($nums as $num) {
+              echo $num . '<br>';
+        }
+      }
+    }
+
+  
+       $array = [15, 4, 18, 23, 10 ];
+       sort_2way($array, true);
+       sort_2way($array, false);
+        ?>
+    </p>
+</body>
+
+</html>
